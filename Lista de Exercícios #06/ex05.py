cc558b0139e46077aac241f05c496ad95b251c91
Pedro Wellinsson <167886193+P-Wellinsson@users.noreply.github.com>
2024-08-14 22:10:50 -0300

    Create ex05.py

---
+++ Lista de Exercícios #06/ex05.py
@@ -0,0 +1,1 @@
+# Escreva um programa que leia um arquivo em python (nome fornecido pelo usuário). O programa deverá informar quantas linhas o arquivo tem e a quantidade de “prints” que o código possui.
