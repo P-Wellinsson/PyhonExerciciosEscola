133ed418f4a8da169e69e5517c97f058040cbf78
Pedro Wellinsson <167886193+P-Wellinsson@users.noreply.github.com>
2024-10-01 09:49:26 -0300

    Create and finalize ex003 of revisão

---
+++ exercicios_revisao/ex003.py
@@ -0,0 +1,6 @@
+# Crie uma lista de palavras e ordene-as alfabeticamente.
+words = ['processador', 'memoria', 'teclado', 'monitor', 'impressora', 'software', 'hardware', 'mouse', 'usb', 'internet', 'rede', 'servidor', 'router', 'sistema', 'windows', 'linux', 'firewall', 'antivirus', 'navegador', 'banco', 'dados', 'arquivos', 'memoria', 'cpu', 'placa', 'video', 'webcam', 'microfone',
+         'celular', 'tablete', 'notebook', 'laptop', 'configuracao', 'codigo', 'programa', 'compilador', 'debug', 'memoria', 'ssd', 'hd', 'armazenamento', 'backup', 'criptografia', 'login', 'senha', 'registro', 'cache', 'driver', 'atualizacao', 'upload', 'download', 'bit', 'byte', 'terabyte', 'gigabyte', 'processo', 'serial']
+# words.sort()
+words = sorted(words)
+print(words)
